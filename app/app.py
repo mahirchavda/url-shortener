09e1a32cc8936fae1263678b1c7a0f89a5a8c2b4
Mahir Chavda <mahir.chavda13@gmail.com>
2021-02-27 22:41:20 +0530

    Complex mapping

--- app/app.py
+++ app/app.py
@@ -15,9 +15,28 @@ def reload():
 
 @app.route("/<nickname>")
 def go_to_nickname(nickname):
-    url = mm.get_long_url(nickname)
-    if not url:
+    nickname = nickname.lower()
+    data = mm._mappi.get(nickname)
+    if not data:
         return "{} nickname does not exist".format(nickname)
+    return redirect(data["default"])
+
+
+@app.route("/<nickname1>/<nickname2>")
+def go_to_nickname12(nickname1, nickname2):
+    nickname1 = nickname1.lower()
+    nickname2 = nickname2.lower()
+
+    data = mm._mappi.get(nickname1)
+    if not data:
+        return "{} nickname does not exist".format(nickname1)
+
+    url = data["default"]
+    data2 = data.get(nickname2)
+    if not data2:
+        url += data.get("suffix", "") + nickname2
+    else:
+        url += data.get("prefix", "") + data2["default"]
     return redirect(url)
 
 
